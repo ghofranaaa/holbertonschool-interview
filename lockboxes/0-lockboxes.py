88ff93a61a9980d612a9360d3dce2de88bac0eb0
ghofranaaa <smailikhaled841@gmail.com>
2025-04-07 08:48:28 +0100

    unlocking boxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+	"""
+	This function checks if all boxes can be opened.
+	"""
+	if not boxes or not isinstance(boxes, list):
+		return False
+
+	num_boxes = len(boxes)
+	visited = set()
+	to_visit = [0]
+
+	while to_visit:
+		current = to_visit.pop()
+		if current not in visited:
+			visited.add(current)
+			for key in boxes[current]:
+				if isinstance(key, int) and 0 <= key < num_boxes and key not in visited:
+					to_visit.append(key)
+
+	return len(visited) == num_boxes
