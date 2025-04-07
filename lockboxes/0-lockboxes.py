405018f8f784030a32fe895c039fb8b0e97328e0
ghofranaaa <smailikhaled841@gmail.com>
2025-04-07 08:59:21 +0100

    fixing indentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,22 +3,22 @@
 
 
 def canUnlockAll(boxes):
-	"""
-	This function checks if all boxes can be opened.
-	"""
-	if not boxes or not isinstance(boxes, list):
-		return False
+    """
+    This function checks if all boxes can be opened.
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
 
-	num_boxes = len(boxes)
-	visited = set()
-	to_visit = [0]
+    num_boxes = len(boxes)
+    visited = set()
+    to_visit = [0]
 
-	while to_visit:
-		current = to_visit.pop()
-		if current not in visited:
-			visited.add(current)
-			for key in boxes[current]:
-				if isinstance(key, int) and 0 <= key < num_boxes and key not in visited:
-					to_visit.append(key)
+    while to_visit:
+        current = to_visit.pop()
+        if current not in visited:
+            visited.add(current)
+            for key in boxes[current]:
+                if isinstance(key, int) and 0 <= key < num_boxes and key not in visited:
+                    to_visit.append(key)
 
-	return len(visited) == num_boxes
+    return len(visited) == num_boxes
