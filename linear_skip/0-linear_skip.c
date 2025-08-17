aa582e04c53c6d28b9811488a413c99057394bea
ghofranaaa <smailikhaled841@gmail.com>
2025-08-18 00:52:18 +0100

    linear skip list

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -4,57 +4,47 @@
  * linear_skip - searches for a value in a sorted skip list
  * @list: pointer to the head of the skip list
  * @value: value to search for
- *
  * Return: pointer to the first node where value is located,
  *         or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *prev = list, *curr = list;
+	skiplist_t *prev = list, *curr = list;
+	skiplist_t *end;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    /* traverse using express lane */
-    while (curr->express && curr->express->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n",
-               curr->express->index, curr->express->n);
-        prev = curr->express;
-        curr = curr->express;
-    }
-
-    /* determine the range to search linearly */
-    if (curr->express)
-        printf("Value checked at index [%lu] = [%d]\n",
-               curr->express->index, curr->express->n);
-
-    if (curr->express)
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               prev->index, curr->express->index);
-    else
-    {
-        /* reach the end of the list */
-        skiplist_t *end = curr;
-        while (end->next)
-            end = end->next;
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               prev->index, end->index);
-    }
-
-    /* linear search between prev and curr->express (or end of list) */
-    curr = prev;
-    while (curr && curr->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-        curr = curr->next;
-    }
-
-    if (curr && curr->n == value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-        return (curr);
-    }
-
-    return (NULL);
+	while (curr->express && curr->express->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       curr->express->index, curr->express->n);
+		prev = curr->express;
+		curr = curr->express;
+	}
+	if (curr->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       curr->express->index, curr->express->n);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       prev->index, curr->express->index);
+		end = curr->express;
+	}
+	else
+	{
+		end = curr;
+		while (end->next)
+			end = end->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       prev->index, end->index);
+	}
+	curr = prev;
+	while (curr && curr->index <= end->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		if (curr->n == value)
+			return (curr);
+		curr = curr->next;
+	}
+	return (NULL);
 }
--- linear_skip/search.h
+++ linear_skip/search.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+
 /**
 * struct skiplist_s - Singly linked list with an express lane
 *
@@ -13,6 +14,9 @@
 * @index: Index of the node in the list
 * @next: Pointer to the next node
 * @express: Pointer to the next node in the express lane
+*
+* Description: singly linked list node structure with an express lane
+* for Holberton project
 */
 typedef struct skiplist_s
 {
@@ -27,6 +31,6 @@ skiplist_t *create_skiplist(int *array, size_t size);
 
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
-skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *linear_skip(skiplist_t *head, int value);
 
 #endif /* _SEARCH_H_ */
