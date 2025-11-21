11bd4c1f87c2e3d16394b66037c67264ea6ad97a
ghofranaaa <smailikhaled841@gmail.com>
2025-11-21 22:46:29 +0100

    substring

--- substring/substring.c
+++ substring/substring.c
@@ -6,84 +6,90 @@
 /* Helper to compare two arrays of word counts */
 int compare_counts(int *a, int *b, int size)
 {
-    for (int i = 0; i < size; i++)
-        if (a[i] != b[i])
-            return 0;
-    return (1);
+	int i;
+
+	for (i = 0; i < size; i++)
+		if (a[i] != b[i])
+			return (0);
+	return (1);
 }
 
 /* Main function */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    if (!s || !words || nb_words == 0)
-    {
-        *n = 0;
-        return (NULL);
-    }
+	int word_len, sub_len, s_len, *res, count, i;
 
-    int word_len = strlen(words[0]);
-    int sub_len = word_len * nb_words;
-    int s_len = strlen(s);
+	if (!s || !words || nb_words == 0)
+	{
+		*n = 0;
+		return (NULL);
+	}
 
-    int *res = malloc(sizeof(int) * s_len);
-    if (!res)
-        return (NULL);
+	word_len = strlen(words[0]);
+	sub_len = word_len * nb_words;
+	s_len = strlen(s);
 
-    int count = 0;
+	res = malloc(sizeof(int) * s_len);
+	if (!res)
+		return (NULL);
 
-    for (int i = 0; i <= s_len - sub_len; i++)
-    {
-        int found = 1;
-        int *used = calloc(nb_words, sizeof(int));
-        if (!used)
-        {
-            free(res);
-            return (NULL);
-        }
+	count = 0;
 
-        for (int j = 0; j < nb_words; j++)
-        {
-            int matched = 0;
-            char *substr = strndup(s + i + j * word_len, word_len);
-            if (!substr)
-            {
-                free(used);
-                free(res);
-                return (NULL);
-            }
+	for (i = 0; i <= s_len - sub_len; i++)
+	{
+		int found = 1, j;
+		int *used = calloc(nb_words, sizeof(int));
 
-            for (int k = 0; k < nb_words; k++)
-            {
-                if (!used[k] && strcmp(substr, words[k]) == 0)
-                {
-                    used[k] = 1;
-                    matched = 1;
-                    break;
-                }
-            }
-            free(substr);
+		if (!used)
+		{
+			free(res);
+			return (NULL);
+		}
 
-            if (!matched)
-            {
-                found = 0;
-                break;
-            }
-        }
+		for (j = 0; j < nb_words; j++)
+		{
+			int matched = 0, k;
+			char *substr = strndup(s + i + j * word_len, word_len);
 
-        free(used);
+			if (!substr)
+			{
+				free(used);
+				free(res);
+				return (NULL);
+			}
 
-        if (found)
-            res[count++] = i;
-    }
+			for (k = 0; k < nb_words; k++)
+			{
+				if (!used[k] && strcmp(substr, words[k]) == 0)
+				{
+					used[k] = 1;
+					matched = 1;
+					break;
+				}
+			}
 
-    if (count == 0)
-    {
-        free(res);
-        *n = 0;
-        return (NULL);
-    }
+			free(substr);
 
-    *n = count;
-    return (res);
+			if (!matched)
+			{
+				found = 0;
+				break;
+			}
+		}
+
+		free(used);
+
+		if (found)
+			res[count++] = i;
+	}
+
+	if (count == 0)
+	{
+		free(res);
+		*n = 0;
+		return (NULL);
+	}
+
+	*n = count;
+	return (res);
 }
-			
