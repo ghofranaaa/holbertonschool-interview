53b80faa3e421da7b3dc8823306f473e2db2fb73
ghofranaaa <smailikhaled841@gmail.com>
2026-01-02 20:04:53 +0100

    binary tree

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -20,5 +20,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif /* BINARY_H*/
